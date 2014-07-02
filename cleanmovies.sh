08fda7eea253e634ef4a4db3f07516f2127e0f54
grervirg <grervirg@gmail.com>
2014-07-01 21:37:20 -0400

    fix cleanup

--- cleanmovies.sh
+++ cleanmovies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #cleans out unneeded files in media
 
-find /media/MyMedia/Movies -type f -name "*.tbn" -exec rm -f {}\;
-find /media/MyMedia/Movies -type f -name "*.db" -exec rm -f {}\;
-find /media/MyMedia/Movies -type f -name "*.idx" -exec rm -f {}\;
+find /media/MyMedia/Movies -type f -name "*.tbn" -delete
+find /media/MyMedia/Movies -type f -name "*.db" -delete
+find /media/MyMedia/Movies -type f -name "*.idx" -delete
 find /media/MyMedia/Movies -type d -empty > /media/MyMedia/Movies/empty.log
