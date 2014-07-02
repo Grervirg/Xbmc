49506b890cdef612bed3de7a633552ff70028800
grervirg <grervirg@gmail.com>
2014-07-01 21:29:02 -0400

    update

--- cleanmovies.sh
+++ cleanmovies.sh
@@ -4,3 +4,4 @@
 find /media/MyMedia/Movies -type f -name "*.tbn" -exec rm -f {}\;
 find /media/MyMedia/Movies -type f -name "*.db" -exec rm -f {}\;
 find /media/MyMedia/Movies -type f -name "*.idx" -exec rm -f {}\;
+find /media/MyMedia/Movies -type d -empty > /media/MyMedia/Movies/empty.log
