770168ea8ded24594c759f76044fe707f5e3ab33
grervirg <grervirg@gmail.com>
2014-07-01 21:19:51 -0400

    update

--- cleanmovies.sh
+++ cleanmovies.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+#cleans out unneeded files in media
+
 find /media/MyMedia/Movies -type f -name "*.tbn" -exec rm -f {}\;
+find /media/MyMedia/Movies -type f -name "*.db" -exec rm -f {}\;
+find /media/MyMedia/Movies -type f -name "*.idx" -exec rm -f {}\;
