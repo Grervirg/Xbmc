87c34b0b88875767b1aee46aa920be5b91374825
Grervirg <Grervirg@gmail.com>
2014-07-07 22:03:34 -0400

    Scrape after Rename

--- ProcessMovies.sh
+++ ProcessMovies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 filebot -rename /media/Downloads/Process/Movies --db IMDB --format "{n} ({y})[{source}]/{n} ({y})-{source}[{vc}]" --output /media/MyMedia/Movies/ --action move
+/opt/tinymedia/tinyMediaManagerCMD.sh -update -scrapeNew
