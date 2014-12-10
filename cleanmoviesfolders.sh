a8af1c70712ed9cb2454503643b2d2aa97f5232a
grervirg <grervirg@gmail.com>
2014-12-10 12:59:15 -0500

    0.0.0.2

--- cleanmoviesfolders.sh
+++ cleanmoviesfolders.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #cleans out unneeded files in media
-# version 0.0.0.1
+# version 0.0.0.2
 
 find /media/MyMedia/Movies -type f -name "*.tbn" -delete
 find /media/MyMedia/Movies -type f -name "*.db" -delete
 find /media/MyMedia/Movies -type f -name "*.idx" -delete
 find /media/MyMedia/Movies -type f -name "*.xml" -delete
 find /media/MyMedia/Movies -type f -name "*.nfo" -delete
+find /media/MyMedia/Movies -type f -name "*.png" -delete
+#find /media/MyMedia/Movies -type f -name "*.nfo" -delete
+#find /media/MyMedia/Movies -type f -name "*.nfo" -delete
 find /media/MyMedia/Movies -type d -empty > /media/MyMedia/Movies/empty.log
 find /media/MyMedia/Movies -type d -empty -delete
---
+++ scrapemovies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#cleans out unneeded files in media
+# version 0.0.0.2
+
+sh /opt/tinymedia/tinyMediaManagerCMD.sh -updateMovies -scrapeNew
