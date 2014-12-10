724970d04928662e40f2e2573c1368a07c33db21
grervirg <grervirg@gmail.com>
2014-12-10 13:24:04 -0500

    0.0.0.2a

---
+++ htpcmovies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Htpc Movie Automation
+# version 0.0.0.2
+
+sh scrapemovies.sh
+sh cleanmoviesfolders.sh
--- scrapemovies.sh
+++ scrapemovies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#cleans out unneeded files in media
+#Updates and scrapes movie files
 # version 0.0.0.2
 
 sh /opt/tinymedia/tinyMediaManagerCMD.sh -updateMovies -scrapeNew
